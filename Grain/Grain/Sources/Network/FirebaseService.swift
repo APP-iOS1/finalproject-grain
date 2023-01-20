2077f169f1602ac80c1b3264255c58101131498a
Wonder Park <pkkyung26@gmail.com>
2023-01-20 15:47:10 +0900

    [Fix] Service 코드수정

--- Grain/Grain/Sources/Network/FirebaseService.swift
+++ Grain/Grain/Sources/Network/FirebaseService.swift
@@ -18,26 +18,88 @@ enum FirebaseService{
     }
     
     
+    // MARK: - 매거진 가져오기
+    func getMagazine() -> AnyPublisher<MagazineResponse, Error> {
+        print("TestService getMagazine start")
+        let firestoreURL = "https://firestore.googleapis.com/v1/projects/grain-final/databases/(default)/documents/Magazine"
+        
+//        var request = URLRequest(url: (URL(string: firestoreURL) ?? URL(string: ""))! )
+        var request = URLRequest(url: URL(string: firestoreURL))
+        
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = HTTPMethod.get.rawValue
+
+        return URLSession
+            .shared
+            .dataTaskPublisher(for: request)
+            .map{ $0.data}
+            .decode(type: MagazineResponse.self, decoder: JSONDecoder())
+            .eraseToAnyPublisher()
+    }
+    
+    // MARK: - 매거진 데이터 넣기
+    func insertMagazine() -> AnyPublisher<MagazineResponse, Error> {
+        let firestoreURL = "https://firestore.googleapis.com/v1/projects/grain-final/databases/(default)/documents"
+        guard let url = URL(string: "\(firestoreURL)/Magazine") else {
+            return
+        }
+
+        var request = URLRequest(url: url)
+        
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = HTTPMethod.post.rawValue   //post 방식
+        request.httpBody = MagazineQuery.insertMagazineQuery(userID: userID, cameraInfo: cameraInfo, nickName: nickName, image: image, content: content, title: title)
+
+        return URLSession
+            .shared
+            .dataTaskPublisher(for: request)
+            .map{ $0.data}
+            .decode(type: MagazineResponse.self, decoder: JSONDecoder())
+            .eraseToAnyPublisher()
+    }
+    
+    
+    // MARK: - 매거진 데이터 넣기
+    func insertMagazine() -> AnyPublisher<MagazineResponse, Error> {
+        let firestoreURL = "https://firestore.googleapis.com/v1/projects/grain-final/databases/(default)/documents"
+        guard let url = URL(string: "\(firestoreURL)/Magazine") else {
+            return
+        }
+
+        var request = URLRequest(url: url)
+        
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = HTTPMethod.post.rawValue   //post 방식
+        request.httpBody = MagazineQuery.insertMagazineQuery(userID: userID, cameraInfo: cameraInfo, nickName: nickName, image: image, content: content, title: title)
+
+        return URLSession
+            .shared
+            .dataTaskPublisher(for: request)
+            .map{ $0.data}
+            .decode(type: MagazineResponse.self, decoder: JSONDecoder())
+            .eraseToAnyPublisher()
+    }
+    
     // MARK: 매거진 데이터 넣기
     static func insertMagazine(userID: String, cameraInfo: String, nickName: String, image: String, content: String, title: String){
         let firestoreURL = "https://firestore.googleapis.com/v1/projects/grain-final/databases/(default)/documents"
         guard let url = URL(string: "\(firestoreURL)/Magazine") else {
             return
         }
-        
+
         var request = URLRequest(url: url )
-        
+
         // MARK: 리퀘스트 헤더 설정
         request.addValue("application/json", forHTTPHeaderField: "Content-Type") //서버에 길이 알림
         request.httpMethod = HTTPMethod.post.rawValue   //post 방식
         request.httpBody = MagazineQuery.insertMagazineQuery(userID: userID, cameraInfo: cameraInfo, nickName: nickName, image: image, content: content, title: title)
-        
-        
+
+
         // 서버 통신
         URLSession.shared.dataTask(with: request) { (data, response, error) in
-            
+
         }.resume()
-        
+
     }
     
     // MARK: 커뮤니티 데이터 넣기
@@ -62,36 +124,5 @@ enum FirebaseService{
         
     }
     
-//    static func getMagazine() {
-//        let firestoreURL = "https://firestore.googleapis.com/v1/projects/grain-final/databases/(default)/documents/Magazine"
-//        guard let url = URL(string: firestoreURL) else {
-//
-//            return
-//        }
-//        var request = URLRequest(url: url)
-//
-//        request.httpMethod = HTTPMethod.get.rawValue
-////        URLSession.shared.dataTask(with: request)
-//
-//        URLSession.shared.dataTask(with: request) { (data, response, error) in
-//
-//            //            // status 코드가 200번대여야 성공적인 네트워크라 판단
-//            //            let successsRange = 200..<300
-//            //            guard let statusCode = (response as? HTTPURLResponse)?.statusCode,
-//            //                  successsRange.contains(statusCode) else { return }
-//            //
-//            guard let data = data, error == nil else{
-//                fatalError("error")
-//            }
-//            //
-//            let response = try? JSONDecoder().decode(MagazineResponse.self, from: data)
-//
-//            if let response = response{
-//
-//                for i in response.magazines{
-//                    print(resultArray)
-//                }
-//            }
-//        }.resume()
-//    }
+
 }

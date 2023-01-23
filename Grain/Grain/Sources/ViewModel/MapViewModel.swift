cea9984ea03e8ff5523a66d550fdb474216dfa4a
Wonder Park <pkkyung26@gmail.com>
2023-01-23 21:27:24 +0900

    [Del] ViewModel 폴더 삭제

--- Grain/Grain/Sources/ViewModel/MapViewModel.swift
+++
@@ -1,8 +0,0 @@
-//
-//  MapViewModel.swift
-//  Grain
-//
-//  Created by 박희경 on 2023/01/20.
-//
-
-import Foundation
--- Grain/Grain/Sources/ViewModel/UserViewModel.swift
+++
@@ -1,8 +0,0 @@
-//
-//  UserViewModel.swift
-//  Grain
-//
-//  Created by 박희경 on 2023/01/20.
-//
-
-import Foundation

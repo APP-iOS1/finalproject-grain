e685312841cf780aa4371cbda697cd4a9f99a095
Wonder Park <pkkyung26@gmail.com>
2023-01-23 21:26:18 +0900

    [feat] ViewModels 추가

---
+++ Grain/Grain/Sources/ViewModels/CommunityViewModel.swift
@@ -0,0 +1,56 @@
+//
+//  CommunityViewModel.swift
+//  Grain
+//
+//  Created by 박희경 on 2023/01/23.
+//
+
+import Foundation
+import Combine
+
+
+final class CommunityViewModel: ObservableObject {
+    
+    var subscription = Set<AnyCancellable>()
+    
+    @Published var communities = [Community]()
+    
+    var fetchCommunitySuccess = PassthroughSubject<(), Never>()
+    var insertCommunitySuccess = PassthroughSubject<(), Never>()
+    
+  
+    func fetchCommunity() {
+        print("CommunityViewModel fetchCommunity Start")
+        
+        CommunityService.getCommunity()
+            .receive(on: DispatchQueue.main)
+            .sink { (completion: Subscribers.Completion<Error>) in
+            print("MagazineViewModel fetchCommunity complete")
+        } receiveValue: { (data: CommunityResponse) in
+            self.communities = data.communities
+            self.fetchCommunitySuccess.send()
+        }.store(in: &subscription)
+    }
+    
+    func insertCommunity() {
+        print("CommunityViewModel insertCommunity Start")
+        
+        CommunityService.insertCommunity()
+            .receive(on: DispatchQueue.main)
+            .sink { (completion: Subscribers.Completion<Error>) in
+            print("MagazineViewModel fetchCommunity complete")
+        } receiveValue: { (data: CommunityResponse) in
+            self.insertCommunitySuccess.send()
+        }.store(in: &subscription)
+    }
+    
+    func updateCommunity() {
+        
+    }
+    
+    func deleteCommunity() {
+        
+    }
+    
+}
+

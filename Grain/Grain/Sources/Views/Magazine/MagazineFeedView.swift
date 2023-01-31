9ff466d0a1c8d697e6eaff60821f8ac28d79aea1
jeonghoonji <wlwjdgns0920@naver.com>
2023-01-31 16:12:32 +0900

    [Feat] FeedView 데이터 연동 완료 : '

--- Grain/Grain/Sources/Views/Magazine/MagazineFeedView.swift
+++ Grain/Grain/Sources/Views/Magazine/MagazineFeedView.swift
@@ -10,76 +10,19 @@ import SwiftUI
 struct MagazineFeedView: View {
     
 //    var index : [MagazineDTO]
+    @ObservedObject var magazineVM = MagazineViewModel()
     
     var body: some View {
         
         ScrollView{
             LazyVStack{
-                
-                Rectangle()
-                    .cornerRadius(15)
-                    .foregroundColor(Color.boxGray)
-                    .overlay(
-                        VStack{
-                            ForEach(0..<2) { _ in
-                                Image("1")
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                                Text("1")
-                                    .font(.title3)
-                                    .bold()
-                                    .foregroundColor(.white)
-                                    .frame(maxWidth:.infinity, alignment:.leading)
-                                Text("1")
-                                    .bold()
-                                    .foregroundColor(Color.white)
-                                    .font(.title)
-                                    .frame(maxWidth:.infinity, alignment:.leading)
-                                    .padding()
-                                    .multilineTextAlignment(.leading)
-                                    .lineLimit(2)
-                                
-                                Text("1")
-                                    .font(.title3)
-                                    .bold()
-                                    .foregroundColor(.white)
-                                    .frame(maxWidth:.infinity, alignment:.leading)
-                                    .padding(.horizontal)
-                                    .lineLimit(1)
-                            }
-//                            Image("1")
-//                                .resizable()
-//                                .aspectRatio(contentMode: .fit)
-//                            Text(index.nickName)
-//                                .font(.title3)
-//                                .bold()
-//                                .foregroundColor(.white)
-//                                .frame(maxWidth:.infinity, alignment:.leading)
-//                                .padding(.horizontal)
-//                            Text(index.title)
-//                                .bold()
-//                                .foregroundColor(Color.white)
-//                                .font(.title)
-//                                .frame(maxWidth:.infinity, alignment:.leading)
-//                                .padding()
-//                                .multilineTextAlignment(.leading)
-//                                .lineLimit(2)
-//
-//                            Text(index.content)
-//                                .font(.title3)
-//                                .bold()
-//                                .foregroundColor(.white)
-//                                .frame(maxWidth:.infinity, alignment:.leading)
-//                                .padding(.horizontal)
-//                                .lineLimit(1)
-                            
-                            
-                        }.cornerRadius(15),
-                        alignment: .top
-                    )
+                ForEach(magazineVM.magazines, id: \.self){ index in
+                    MagazineViewCell(index: index)
+                }
                 
             }
-            MagazineViewCell()
+        }.onAppear{
+            magazineVM.fetchMagazine()
         }
     }
 }

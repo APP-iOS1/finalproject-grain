7466a3fa42ea0c2c2d90bf6b28ae58aad851c81f
jeonghoonji <wlwjdgns0920@naver.com>
2023-02-03 16:48:35 +0900

    [Feat]#55 유저 데이터 불러오기 초기 단계

--- Grain/Grain/Sources/ViewModels/UserViewModel.swift
+++ Grain/Grain/Sources/ViewModels/UserViewModel.swift
@@ -18,13 +18,14 @@ import Combine
 
 
 final class UserViewModel: ObservableObject {
-    
+ 
     var subscription = Set<AnyCancellable>()
     
     @Published var users = [UserDocument]()
     
     // 실패한거 같음
-//    @Published var currentUsers = CurrentUserFields
+    @Published var currentUsers : CurrentUserFields?
+    
     
     var fetchUsersSuccess = PassthroughSubject<(), Never>()
     var insertUsersSuccess = PassthroughSubject<(), Never>()
@@ -40,23 +41,23 @@ final class UserViewModel: ObservableObject {
 
     }
     // 실패한거 같음
-//    func fetchCurrentUser(userID: String) {
-//        UserService.getCurrentUser(userID: userID)
-//            .receive(on: DispatchQueue.main)
-//            .sink { (completion: Subscribers.Completion<Error>) in
-//        } receiveValue: { (data: CurrentUserResponse) in
-//            self.currentUsers = data.fields
-//            self.fetchUsersSuccess.send()
-//        }.store(in: &subscription)
-//
-//    }
+    
+    func fetchCurrentUser(userID: String) {
+        UserService.getCurrentUser(userID: userID)
+            .receive(on: DispatchQueue.main)
+            .sink { (completion: Subscribers.Completion<Error>) in
+        } receiveValue: { (data: CurrentUserResponse) in
+            self.currentUsers = data.fields
+            self.fetchUsersSuccess.send()
+        }.store(in: &subscription)
+
+    }
     
     func insertUser(myFilm: String,bookmarkedMagazineID: String,email: String,myCamera: String,postedCommunityID: String,postedMagazineID: String,likedMagazineId: String,lastSearched: String,bookmarkedCommunityID: String,recentSearch: String,id: String,following: String,myLens : String,profileImage: String,name: String,follower: String,nickName: String) {
 
         UserService.insertUser(myFilm: myFilm,bookmarkedMagazineID: bookmarkedMagazineID,email: email,myCamera: myCamera,postedCommunityID: postedCommunityID,postedMagazineID: postedMagazineID,likedMagazineId: likedMagazineId,lastSearched: lastSearched,bookmarkedCommunityID: bookmarkedCommunityID,recentSearch: recentSearch,id: id,following: following,myLens :myLens,profileImage: profileImage,name: name,follower: follower,nickName: nickName)
             .receive(on: DispatchQueue.main)
             .sink { (completion: Subscribers.Completion<Error>) in
-//            print("MagazineViewModel fetchCommunity complete")
         } receiveValue: { (data: UserResponse) in
             self.fetchUsersSuccess.send()
         }.store(in: &subscription)

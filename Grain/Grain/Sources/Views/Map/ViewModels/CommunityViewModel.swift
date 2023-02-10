3f9f6434c999bf95332a39166788ea4dfec700ca
Wonder Park <pkkyung26@gmail.com>
2023-02-10 16:55:11 +0900

    [Feat] Community VM 구현

--- Grain/Grain/Sources/Views/Map/ViewModels/CommunityViewModel.swift
+++ Grain/Grain/Sources/Views/Map/ViewModels/CommunityViewModel.swift
@@ -5,14 +5,12 @@
 //  Created by 박희경 on 2023/01/23.
 //
 
-// [TODO]
-// 1. storage 사진 올리기
-// 2. realtime database
 
 import Foundation
 import Combine
 import FirebaseFirestore
 
+// FIXME: (희경) ViewModel 코드 구조화 할 수 있을거같음. 같은 코드 중복되는부분 나중에 리펙토링해보자.
 final class CommunityViewModel: ObservableObject {
     
     var subscription = Set<AnyCancellable>()
@@ -21,20 +19,48 @@ final class CommunityViewModel: ObservableObject {
     
     var fetchCommunitySuccess = PassthroughSubject<[CommunityDocument], Never>()
     var insertCommunitySuccess = PassthroughSubject<(), Never>()
+    var updateCommunitySuccess = PassthroughSubject<(), Never>()
+    var deleteCommunitySuccess = PassthroughSubject<(), Never>()
     
+    //MARK: - 커뮤니티 데이터 가져오기 메소드
     func fetchCommunity() {
-//        print("CommunityViewModel fetchCommunity Start")
-        
         CommunityService.getCommunity()
             .receive(on: DispatchQueue.main)
             .sink { (completion: Subscribers.Completion<Error>) in
-//            print("CommunityViewModel fetchCommunity complete")
-        } receiveValue: { (data: CommunityResponse) in
-            self.communities = data.documents
-            self.fetchCommunitySuccess.send(data.documents)
-        }.store(in: &subscription)
-        
-        
+            } receiveValue: { (data: CommunityResponse) in
+                self.communities = data.documents
+                self.fetchCommunitySuccess.send(data.documents)
+            }.store(in: &subscription)
+    }
+    
+    //MARK: - 커뮤니티 데이터 올리기 메소드
+    func insertCommunity(data: CommunityFields, images: [UIImage]) {
+        CommunityService.insertCommunity(data: data, images: images)
+            .receive(on: DispatchQueue.main)
+            .sink { (completion: Subscribers.Completion<Error>) in
+            } receiveValue: { (data: CommunityResponse) in
+                self.insertCommunitySuccess.send()
+            }.store(in: &subscription)
+    }
+    
+    //MARK: - 커뮤니티 데이터 수정 메소드
+    func updateCommunity(data: CommunityDocument, docID: String) {
+        CommunityService.updateCommunity(data: data, docID: docID)
+            .receive(on: DispatchQueue.main)
+            .sink { (completion: Subscribers.Completion<Error>) in
+            } receiveValue: { (data: CommunityResponse) in
+                self.updateCommunitySuccess.send()
+            }.store(in: &subscription)
+    }
+    
+    //MARK: - 커뮤니티 데이터 삭제 메소드
+    func deleteCommunity(docID: String) {
+        CommunityService.deleteMagazine(docID: docID)
+            .receive(on: DispatchQueue.main)
+            .sink { (completion: Subscribers.Completion<Error>) in
+            } receiveValue: { (data: CommunityResponse) in
+                self.deleteCommunitySuccess.send()
+            }.store(in: &subscription)
     }
     
     // 카테고리별 데이터를 filtering 해서 리턴하는 함수
@@ -45,17 +71,6 @@ final class CommunityViewModel: ObservableObject {
         return categoryData
     }
     
-    func insertCommunity(profileImage: String, nickName: String, category: String, image: String, userID: String, title: String, content: String) {
-//        print("CommunityViewModel insertCommunity Start")
-
-        CommunityService.insertCommunity(profileImage: profileImage, nickName: nickName, category: category, image: image, userID: userID, title: title, content: content)
-            .receive(on: DispatchQueue.main)
-            .sink { (completion: Subscribers.Completion<Error>) in
-//            print("CommunityViewModel fetchCommunity complete")
-        } receiveValue: { (data: CommunityResponse) in
-            self.insertCommunitySuccess.send()
-        }.store(in: &subscription)
-    }
     
     // MARK: Update -> Firebase Store SDK 사용
     func updateCommunitySDK(updateDocument: String, updateKey: String, updateValue: String, isArray: Bool) async {
@@ -76,7 +91,7 @@ final class CommunityViewModel: ObservableObject {
             do{
                 try? await documentRef.updateData(
                     [
-                         "\(updateKey)" : "\(updateValue)"
+                        "\(updateKey)" : "\(updateValue)"
                     ]
                 )
             }catch let error {
@@ -89,7 +104,7 @@ final class CommunityViewModel: ObservableObject {
     func deleteCommunitySDK(updateDocument: String, deleteKey: String, deleteIndex: String, isArray: Bool) async {
         let db = Firestore.firestore()
         let documentRef = db.collection("Community").document("\(updateDocument)")
-    
+        
         if isArray{
             do{
                 try? await documentRef.updateData(

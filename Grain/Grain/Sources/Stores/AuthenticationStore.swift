2d99297c34ebfabe3e5c0606f54b78d556a032ad
cho407 <parfume407@gmail.com>
2023-02-11 07:21:42 +0900

    [Feat] #109 애플 로그인 구현 완료
    회원 탈퇴, 최초로그인 판별 추가 필요

--- Grain/Grain/Sources/Stores/AuthenticationStore.swift
+++ Grain/Grain/Sources/Stores/AuthenticationStore.swift
@@ -11,13 +11,32 @@ import Firebase
 import GoogleSignIn
 import GoogleSignInSwift
 import Combine
+import AuthenticationServices
+import CryptoKit
 
+/// 로그인 상태관리
+enum AuthenticationState {
+    case unauthenticated
+    case authenticating
+    case authenticated
+}
+/// 로그인 회사 상태관리
+enum LogInCompanyState{
+    case googleLogIn
+    case kakaoLogIn
+    case appleLogIn
+    case noCompany
+}
 
 final class AuthenticationStore: ObservableObject {
+    
+    
     @Published var authenticationState: AuthenticationState = .unauthenticated
     @Published var logInCompanyState: LogInCompanyState = .noCompany
     @Published var user: User?
     @Published var displayName = ""
+    @Published var errorMessage = ""
+    fileprivate var currentNonce: String?
     
     let userDatabasePath = Firestore.firestore().collection("User")
     let authPath = Auth.auth()
@@ -46,7 +65,7 @@ final class AuthenticationStore: ObservableObject {
             GIDSignIn.sharedInstance.restorePreviousSignIn { [unowned self] user, error in
                 authenticateUser(for: user, with: error)
             }
-        } else {
+        } else {//첫 로그인
             guard let clientID = FirebaseApp.app()?.options.clientID else { return }
             
             let configuration = GIDConfiguration(clientID: clientID)
@@ -81,17 +100,101 @@ final class AuthenticationStore: ObservableObject {
                 
                 insertUser(myFilm: "선택", bookmarkedMagazineID: "", email: profile.email, myCamera: "필수", postedCommunityID: "", postedMagazineID: "", likedMagazineId: "", lastSearched: "", bookmarkedCommunityID: "", recentSearch: "", id: uid ?? "", following: "", myLens: "선택", profileImage: "", name: profile.name, follower: "", nickName: "")
                 
-                self.authStateAuthenticated(user: CurrentUser(id: uid ?? "", name: profile.name, email: profile.email))
+                self.authenticationState = .authenticating
                 self.logInCompanyState = .googleLogIn
             }
         }
     }
-    // MARK: - Google LogOut
     
-    /// 구글 로그아웃
-    public func googleLogout() {
-        GIDSignIn.sharedInstance.signOut()
-        
+    // MARK: - Apple LogIn
+    /// 애플 로그인 request
+    func hadleSignInwithAppleRequest(_ request: ASAuthorizationAppleIDRequest){
+        request.requestedScopes = [.fullName, .email]
+        let nonce = randomNonceString()
+         currentNonce = nonce
+        request.nonce = sha256(nonce)
+    }
+    /// 애플 로그인 completion result
+    func handleSignInWithAppleCompletion(_ result: Result<ASAuthorization, Error>){
+        if case .failure(let failure) = result {
+            errorMessage = failure.localizedDescription
+        }
+        else if case . success(let success) = result {
+            if let appleIDCredential = success.credential as? ASAuthorizationAppleIDCredential{
+                guard let nonce = currentNonce else {
+                    fatalError("Invalid state: a login callback was receiced, but no login request was sent.")
+                }
+                guard let appleIDToken = appleIDCredential.identityToken else {
+                    print("Unable to fetch identity token.")
+                    return
+                }
+                guard let idTokenString = String(data: appleIDToken, encoding: .utf8) else {
+                    print("Unable to serialise token string from data: \(appleIDToken.debugDescription)")
+                    return
+                }
+                let credential = OAuthProvider.credential(withProviderID: "apple.com", idToken: idTokenString, rawNonce: nonce)
+                
+                
+                Task{
+                    do{
+                        let result = try await Auth.auth().signIn(with: credential)
+                        // diplayName update 시점
+                        await updateDisplayName(for: result.user, with: appleIDCredential)
+                        self.logInCompanyState = .appleLogIn
+                    }
+                    catch{
+                        print("Error authenticating: \(error.localizedDescription)")
+                    }
+                    
+                }
+            }
+        }
+    }
+    /// displayName update
+    func updateDisplayName(for user: User, with appleIDCredential: ASAuthorizationAppleIDCredential, force: Bool = false) async {
+       if let currentDisplayName = Auth.auth().currentUser?.displayName, !currentDisplayName.isEmpty {
+         // current user is non-empty, don't overwrite it
+       }
+       else {
+         let changeRequest = user.createProfileChangeRequest()
+         changeRequest.displayName = appleIDCredential.displayName()
+         do {
+           try await changeRequest.commitChanges()
+           self.displayName = Auth.auth().currentUser?.displayName ?? ""
+         }
+         catch {
+           print("Unable to update the user's displayname: \(error.localizedDescription)")
+           errorMessage = error.localizedDescription
+         }
+       }
+     }
+    
+    func verifySignInWithAppleAuthenticationState() {
+      let appleIDProvider = ASAuthorizationAppleIDProvider()
+      let providerData = Auth.auth().currentUser?.providerData
+      if let appleProviderData = providerData?.first(where: { $0.providerID == "apple.com" }) {
+        Task {
+          do {
+            let credentialState = try await appleIDProvider.credentialState(forUserID: appleProviderData.uid)
+            switch credentialState {
+            case .authorized:
+              break // The Apple ID credential is valid.
+            case .revoked, .notFound:
+              // The Apple ID credential is either revoked or was not found, so show the sign-in UI.
+              self.appleLogout()
+            default:
+              break
+            }
+          }
+          catch {
+          }
+        }
+      }
+    }
+    
+    // MARK: - Apple LogOut
+    // 기기에서 로그인 하는것이기 때문에 shared instance 까지 로그아웃 시키지 않음
+    public func appleLogout() {
         do {
             try authPath.signOut()
             self.authenticationState = .unauthenticated
@@ -100,26 +203,100 @@ final class AuthenticationStore: ObservableObject {
             print(error.localizedDescription)
         }
     }
+    // MARK: - Google LogOut
     
-    public func authStateAuthenticated(user: CurrentUser) {
-        self.authenticationState = .authenticated
+    /// 구글 로그아웃
+    public func googleLogout() {
+        
+        do {
+            try authPath.signOut()
+            GIDSignIn.sharedInstance.signOut()
+            self.authenticationState = .unauthenticated
+            self.logInCompanyState = .noCompany
+        } catch {
+            print(error.localizedDescription)
+        }
     }
-    // 유저 데이터 만들기
-
+    // MARK: - 회원탈퇴 확인해봐야함
+    func googleDisconnect() {
+        GIDSignIn.sharedInstance.disconnect()
+     }
+    func deleteAccount() async -> Bool {
+       do {
+         try await user?.delete()
+         return true
+       }
+       catch {
+         errorMessage = error.localizedDescription
+         return false
+       }
+     }
+    // MARK: - 유저 데이터 만들기
+    
     var subscription = Set<AnyCancellable>()
     var fetchUsersSuccess = PassthroughSubject<(), Never>()
     func insertUser(myFilm: String,bookmarkedMagazineID: String,email: String,myCamera: String,postedCommunityID: String,postedMagazineID: String,likedMagazineId: String,lastSearched: String,bookmarkedCommunityID: String,recentSearch: String,id: String,following: String,myLens : String,profileImage: String,name: String,follower: String,nickName: String) {
-
+        
         UserService.insertUser(myFilm: myFilm,bookmarkedMagazineID: bookmarkedMagazineID,email: email,myCamera: myCamera,postedCommunityID: postedCommunityID,postedMagazineID: postedMagazineID,likedMagazineId: likedMagazineId,lastSearched: lastSearched,bookmarkedCommunityID: bookmarkedCommunityID,recentSearch: recentSearch,id: id,following: following,myLens :myLens,profileImage: profileImage,name: name,follower: follower,nickName: nickName)
             .receive(on: DispatchQueue.main)
             .sink { (completion: Subscribers.Completion<Error>) in
-        } receiveValue: { (data: UserDocument) in
-            self.fetchUsersSuccess.send()
-        }.store(in: &subscription)
+            } receiveValue: { (data: UserDocument) in
+                self.fetchUsersSuccess.send()
+            }.store(in: &subscription)
     }
+    // MARK: - 애플 로그인 helper
+    private func randomNonceString(length: Int = 32) -> String {
+      precondition(length > 0)
+      let charset: [Character] =
+        Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
+      var result = ""
+      var remainingLength = length
 
-   
+      while remainingLength > 0 {
+        let randoms: [UInt8] = (0 ..< 16).map { _ in
+          var random: UInt8 = 0
+          let errorCode = SecRandomCopyBytes(kSecRandomDefault, 1, &random)
+          if errorCode != errSecSuccess {
+            fatalError(
+              "Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)"
+            )
+          }
+          return random
+        }
+
+        randoms.forEach { random in
+          if remainingLength == 0 {
+            return
+          }
+
+          if random < charset.count {
+            result.append(charset[Int(random)])
+            remainingLength -= 1
+          }
+        }
+      }
+
+      return result
+    }
     
+    private func sha256(_ input: String) -> String {
+      let inputData = Data(input.utf8)
+      let hashedData = SHA256.hash(data: inputData)
+      let hashString = hashedData.compactMap {
+        String(format: "%02x", $0)
+      }.joined()
+
+      return hashString
+    }
+    
+}
+
+extension ASAuthorizationAppleIDCredential {
+  func displayName() -> String {
+    return [self.fullName?.givenName, self.fullName?.familyName]
+      .compactMap( {$0})
+      .joined(separator: " ")
+  }
 }
 
 extension AuthenticationStore {
@@ -131,18 +308,3 @@ extension AuthenticationStore {
         logInCompanyState = .noCompany
     }
 }
-
-/// 로그인 상태관리
-enum AuthenticationState {
-    case unauthenticated
-    case authenticating
-    case authenticated
-}
-
-enum LogInCompanyState{
-    case googleLogIn
-    case kakaoLogIn
-    case noCompany
-}
-
-
--- Grain/Grain/Sources/Views/AuthenticationViews/AuthenticationView.swift
+++ Grain/Grain/Sources/Views/AuthenticationViews/AuthenticationView.swift
@@ -9,47 +9,90 @@ import SwiftUI
 
 import GoogleSignInSwift
 import FirebaseAuth
+import _AuthenticationServices_SwiftUI
 
 struct AuthenticationView: View {
     @EnvironmentObject var authenticationStore: AuthenticationStore
     @EnvironmentObject var kakaoAuthenticationStore: KakaoAuthenticationStore
     
+    @Environment(\.colorScheme) var colorScheme
+    
     var body: some View {
         VStack{
+            Spacer()
             Text("GRAIN")
                 .font(.largeTitle)
                 .bold()
                 .kerning(7)
             Spacer()
-                .frame(maxHeight: Screen.maxHeight * 0.3 )
+                .frame(maxHeight: Screen.maxHeight * 0.25 )
             VStack(alignment: .leading) {
                 Text("환영해요!")
-                    .font(.title2)
+                    .font(.title)
                     .fontWeight(.semibold)
+                   
                 Text("로그인해서 Grain과의 여정을 시작하세요.")
-            }
-            GoogleSignInButton(style:.standard) {
-                authenticationStore.googleLogin()
-            }
-            .frame(maxWidth: Screen.maxWidth * 0.8)
             
+
+            }
+            .padding(.bottom)
+            // MARK: - 애플 로그인
+            SignInWithAppleButton { request in
+                authenticationStore.hadleSignInwithAppleRequest(request)
+            } onCompletion: { result in
+                authenticationStore.handleSignInWithAppleCompletion(result)
+            }
+            .signInWithAppleButtonStyle(colorScheme == .light ? .black : .white)
+            .frame(width: Screen.maxWidth * 0.8, height: Screen.maxHeight * 0.06)
+            .padding(.bottom, 7)
+            
+            // MARK: - 구글 로그인
+            Button {
+                authenticationStore.googleLogin()
+                
+            } label: {
+                RoundedRectangle(cornerRadius: 7)
+                    .stroke(lineWidth: 1.3)
+                    .foregroundColor(.gray)
+                    .overlay{
+                        ZStack {
+                            HStack{
+                                Image("googleLogo")
+                                    .resizable()
+                                    .cornerRadius(4)
+                                    .aspectRatio(contentMode: .fit)
+                                    .padding([.top,.leading, .bottom], 10)
+                                    .padding(.trailing, 3)
+                                Text("Sign in with Google")
+                                    .font(.subheadline)
+                                    .foregroundColor(.middleDarkGray)
+                                    .bold()
+                                
+                                Spacer()
+                                
+                            }
+                        }
+                    }
+                
+            }
+            .frame(width: Screen.maxWidth * 0.8, height: Screen.maxHeight * 0.06)
+            .padding(.bottom, 7)
+            
+            // MARK: - 카카오 로그인
             Button{
                 kakaoAuthenticationStore.kakaoSignIn()
                 authenticationStore.changeLogInCompanyToKakao()
             } label: {
                 Image("kakaoLoginLarge")
                     .resizable()
-                    .frame(maxWidth: Screen.maxWidth * 0.8, maxHeight: Screen.maxHeight * 0.05)
-                    .shadow(radius: 7)
+                    .aspectRatio(contentMode: .fit)
+                  
+                    .cornerRadius(8)
             }
-            Button {
-                print(Auth.auth().currentUser?.email ?? "")
-                print(authenticationStore.logInCompanyState)
-            } label: {
-                Text("프린트")
-            }
+            .frame(width: Screen.maxWidth * 0.8, height: Screen.maxHeight * 0.06)
+            .padding(.bottom, 5)
             Spacer()
-                .frame(maxHeight: Screen.maxHeight * 0.1 )
+                .frame(maxHeight: Screen.maxHeight * 0.12 )
             
         }
     }

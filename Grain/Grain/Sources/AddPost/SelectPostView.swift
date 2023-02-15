8e956bcd8bb63d4ee199af84e04100ecbfd2de5c
Seungsoo Han <hanseu96@gmail.com>
2023-02-15 10:06:53 +0900

    [Fix]#116 애드뷰 동작오류 수정

--- Grain/Grain/Sources/AddPost/SelectPostView.swift
+++ Grain/Grain/Sources/AddPost/SelectPostView.swift
@@ -17,51 +17,24 @@ struct SelectPostView: View {
     @Binding var presented : Bool
     @StateObject var communityVM: CommunityViewModel
     @State var updateNumber : NMGLatLng
-    
+    //@Binding var modalSize: CGFloat
     var body: some View {
         NavigationView {
             VStack {
                 
-                HStack {
-                    Text("나만의 경험을 사람들과 공유해보세요")
-                        .font(.title)
-                        .foregroundColor(.black)
-                        .bold()
-                    Spacer()
-                }
-                .padding(.leading)
-                .padding(.top)
+                Text("글쓰기 설명 text")
                                 
                 //MARK: 매거진 작성 네비게이션 링크
                 NavigationLink {
                     MagazineContentAddView(presented: $presented, updateNumber: updateNumber)
                         .navigationBarBackButtonHidden(true)
                 } label: {
-                    Image("magazineSelect")
-                        .resizable()
-                        .frame(width: Screen.maxWidth * 0.8, height: Screen.maxHeight * 0.25)
-                        .aspectRatio(contentMode: .fit)
-                        .cornerRadius(12)
-                        .blur(radius: 0.5)
-                        .shadow(radius: 2)
-                        .overlay {
-                            VStack(alignment: .leading) {
-                                Spacer()
-                                Text("매거진 작성하기")
-                                    .font(.title)
-                                    .foregroundColor(.white)
-                                    .bold()
-                                    .opacity(1)
-                                    .shadow(radius: 1)
-                                Text("에디터가 되어 사람들과 공유해보세요")
-                                    .font(.callout)
-                                    .foregroundColor(.white)
-                                    .bold()
-                                    .opacity(1)
-                                    .shadow(radius: 1)
-                                Spacer()
-                            }
-                        }
+                    VStack {
+                        Rectangle()
+                            .frame(width: Screen.maxWidth * 0.85, height: Screen.maxHeight * 0.1)
+                        Text("매거진 연결")
+                        Text("매거진test")
+                    }
                 }
                 .padding(.vertical)
                 
@@ -70,39 +43,18 @@ struct SelectPostView: View {
                     AddCommunityView(communityVM: communityVM, presented: $presented)
                         .navigationBarBackButtonHidden(true)
                 } label: {
-                    Image("communitySelect")
-                        .resizable()
-                        .frame(width: Screen.maxWidth * 0.8, height: Screen.maxHeight * 0.25)
-                        .aspectRatio(contentMode: .fit)
-                        .cornerRadius(12)
-                        .blur(radius: 0.5)
-                        .shadow(radius: 2)
-                        .overlay {
-                            VStack(alignment: .leading) {
-                                Spacer()
-                                Text("커뮤니티 작성하기")
-                                    .font(.title)
-                                    .foregroundColor(.white)
-                                    .bold()
-                                    .opacity(1)
-                                    .shadow(radius: 1)
-                                Text("사람들을 만나고 의견을 공유해보세요")
-                                    .font(.callout)
-                                    .foregroundColor(.white)
-                                    .bold()
-                                    .opacity(1)
-                                    .shadow(radius: 1)
-                                Spacer()
-                            }
-                        }
+                    VStack {
+                        Rectangle()
+                            .frame(width: Screen.maxWidth * 0.85, height: Screen.maxHeight * 0.1)
+                        Text("커뮤니티 연결")
+                        Text("커뮤니티 test")
+                    }
                 }
                 .padding(.vertical)
-                
-                Spacer()
-                
+                                
             }
             .toolbar {
-                ToolbarItem(placement: .navigationBarLeading) {
+                ToolbarItem(placement: .navigationBarTrailing) {
                     Button {
                         presented.toggle()
                     } label: {

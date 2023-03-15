91445a2ae62ddbbf859a1cbb25f2b9d4237d4ec8
jeonghoonji <wlwjdgns0920@naver.com>
2023-03-15 13:40:56 +0900

    [Feat]지도 현재 재검색 기능 구현, 좋아요 삭제

--- Grain/Grain/Sources/Views/Map/NearbyPostsComponent.swift
+++ Grain/Grain/Sources/Views/Map/NearbyPostsComponent.swift
@@ -14,6 +14,7 @@ struct NearbyPostsComponent: View {
     @Binding var isShowingPhotoSpot : Bool
     var  nearbyMagazineData : [MagazineDocument]    // 값을 받아옴
     @Binding var clikedMagazineData : MagazineDocument?
+    @Binding var showResearchButton : Bool
     
     @StateObject var userVM = UserViewModel()
     
@@ -28,18 +29,26 @@ struct NearbyPostsComponent: View {
                             .overlay{
                                 VStack{
                                     HStack{
+                                        Spacer()
                                         Image(systemName: "x.circle")
                                             .foregroundColor(.black)
-                                            .position(CGPoint(x: Screen.maxWidth * 0.715 , y: Screen.maxHeight * 0.02))
+    //                                        .position(CGPoint(x: Screen.maxWidth * 0.7 , y: Screen.maxHeight * 0.07))
+                                            .padding(.trailing,5)
+                                            .padding(.top, 5)
                                             .onTapGesture {
-                                                isShowingPhotoSpot.toggle()
+                                                isShowingPhotoSpot = false
+                                                showResearchButton.toggle()
                                             }
+                                    }
+                                    
+                                    HStack{
+                                        Spacer()
                                         KFImage(URL(string: item.fields.image.arrayValue.values[0].stringValue) ?? URL(string:"https://cdn.travie.com/news/photo/202108/21951_11971_5847.jpg"))
                                             .resizable()
                                             .aspectRatio(contentMode: .fit)
                                             .cornerRadius(15)
-                                            .frame(width:Screen.maxWidth * 0.25 ,height: Screen.maxHeight * 0.25)
-                                        
+                                            .offset(y: -10)
+                                        Spacer()
                                         VStack(alignment: .leading){
                                             Text(item.fields.title.stringValue)
                                                 .lineLimit(2)   // 적용이 될지
@@ -51,21 +60,7 @@ struct NearbyPostsComponent: View {
                                                 .foregroundColor(.gray)
                                             
                                             HStack{
-                                                Button{
-                                                    isliked.toggle()
-                                                } label: {
-                                                    if isliked {
-                                                        Image(systemName: "heart.fill")
-                                                            .foregroundColor(.red)
-                                                            .font(.title2)
-                                                    } else {
-                                                        Image(systemName: "heart")
-                                                            .foregroundColor(.black)
-                                                            .font(.title2)
-                                                    }
-                                                }
-                                                
-                                                
+
                                                 NavigationLink {
                                                     MagazineDetailView(userVM: userVM, currentUsers: userVM.currentUsers, data: item)
                                                 }  label: {
@@ -87,7 +82,7 @@ struct NearbyPostsComponent: View {
                                                         .onTapGesture {
                                                             // MARK: Bool 값을 넘겨 지도 상에서 디테일 뷰 보여주기
                                                             clikedMagazineData = item
-                                                            isShowingPhotoSpot.toggle()
+                                                            isShowingPhotoSpot = false
                                                             visitButton.toggle()
                                                             
                                                         }
@@ -95,14 +90,16 @@ struct NearbyPostsComponent: View {
                                                 
                                             }
                                         }
-                                        .padding()
+                                        Spacer()
                                     }
-                                    .frame(width: Screen.maxWidth * 0.75, height: Screen.maxWidth * 0.3)
+
                                 }
                                 RoundedRectangle(cornerRadius: 10)
                                     .stroke(.black, lineWidth: 2)
+                                    .frame(width: Screen.maxWidth * 0.75, height: Screen.maxWidth * 0.3)
                                 
-                            }.padding()
+                            }
+                            .padding()
 
                 }
                 
@@ -113,10 +110,3 @@ struct NearbyPostsComponent: View {
     
     
 }
-
-
-//struct NearbyPostsComponent_Previews: PreviewProvider {
-//    static var previews: some View {
-//        NearbyPostsComponent()
-//    }
-//}

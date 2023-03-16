1e24db16823c1aaa7b0396db1b2b563b24862675
sumanhong <suman0204@naver.com>
2023-03-16 17:00:46 +0900

    [Feat] Network connection check
    네트워크 연결 여부에 따라 안내 화면 나타남

--- Grain/Grain/Sources/Views/ContentView.swift
+++ Grain/Grain/Sources/Views/ContentView.swift
@@ -41,120 +41,129 @@ struct ContentView: View {
         return locationManager.lastLocation?.coordinate.longitude ?? 126.9835183
     }
 
+    // 네트워크 감지
+    @ObservedObject var networkManager = NetworkManager()
+
     var body: some View {
-        VStack{
-            switch authenticationStore.authenticationState {
-            case .unauthenticated, .authenticating:
-                NavigationStack{
-                    AuthenticationView()
-                }
-                
-            case.authenticated:
+        Group{
+            if networkManager.isConnected {
                 VStack{
-                    Spacer()
-                    ZStack {
-                        Spacer()
-//                            .sheet(isPresented: $presented, content: {
-//                                SelectPostView(presented: $presented, communityVM: communityVM, updateNumber: updateNumber, modalSize: $modalSize)
-//                                    .presentationDetents([.height(modalSize)])
-//                            })
-                            .fullScreenCover(isPresented: $presented) {
-                                VStack {
-                                    SelectPostView(presented: $presented,
-                                                   communityVM: communityVM,
-                                                   updateNumber: updateNumber,userLatitude: userLatitude , userLongitude: userLongitude)
-                                }
-                            }
-                        
-                        switch selectedIndex {
-                        case 0:
-                            NavigationStack {
-                                MagazineMainView(userViewModel: userVM, magazineVM: magazineVM)
-                            }
-                        case 1:
-                            NavigationStack {
-                                CommunityView(communityVM: communityVM)
-                            }
-                        case 2:
-                            NavigationStack {
-                                CommunityView(communityVM: communityVM)
-                            }
-                        case 3:
-                            NavigationStack {
-                                MapView(clikedMagazineData: clikedMagazineData, userLatitude: userLatitude, userLongitude: userLongitude)
-                            }
-                        case 4:
-                            NavigationStack {
-                                MyPageView(magazineDocument: magazineVM.userPostsFilter(magazineData: magazineVM.magazines, userPostedArr: userVM.postedMagazineID), boomarkedMagazineDocument: magazineVM.userBookmarkedPostsFilter(magazineData: magazineVM.magazines, userBookmarkedPostedArr: userVM.bookmarkedMagazineID), bookmarkedCommunityDoument: communityVM.userBookmarkedCommunityFilter(communityData: communityVM.communities, userBookmarkedCommunityArr: userVM.bookmarkedCommunityID))
-                            }
-                        default:
-                            NavigationStack {
-                                VStack {
-                                    SelectPostView(presented: $presented,
-                                                   communityVM: communityVM,
-                                                   updateNumber: updateNumber,userLatitude: userLatitude , userLongitude: userLongitude)
-                                }
-                            }
+                    switch authenticationStore.authenticationState {
+                    case .unauthenticated, .authenticating:
+                        NavigationStack{
+                            AuthenticationView()
                         }
-                        Spacer()
-                    }
-                    if isZooming == false {
                         
-                        HStack {
-                            ForEach(0..<5, id: \.self) { number in
-                                Button {
-                                    if number == 2 {
-                                        presented.toggle()
-                                    } else {
-                                        self.selectedIndex = number
-                                    }
-                                } label: {
-                                    if number == 2 {
-                                        Image(systemName: icons[number])
-                                            .font(.title2)
-                                            .foregroundColor(.white)
-                                            .frame(width: 60, height: 60)
-                                            .background(.black)
-                                            .cornerRadius(30)
-                                    }
-                                    else {
-                                        VStack{
-                                            Image(systemName: icons[number])
-                                                .font(.title2)
-                                                .foregroundColor(selectedIndex == number ? .black : Color(UIColor.lightGray))
-                                            Text(labels[number])
-                                                .font(.caption)
-                                                .foregroundColor(selectedIndex == number ? .black : Color(UIColor.lightGray))
+                    case.authenticated:
+                        VStack{
+                            Spacer()
+                            ZStack {
+                                Spacer()
+                                //                            .sheet(isPresented: $presented, content: {
+                                //                                SelectPostView(presented: $presented, communityVM: communityVM, updateNumber: updateNumber, modalSize: $modalSize)
+                                //                                    .presentationDetents([.height(modalSize)])
+                                //                            })
+                                    .fullScreenCover(isPresented: $presented) {
+                                        VStack {
+                                            SelectPostView(presented: $presented,
+                                                           communityVM: communityVM,
+                                                           updateNumber: updateNumber,userLatitude: userLatitude , userLongitude: userLongitude)
                                         }
-                                        .frame(width: 67, height: 60)
+                                    }
+                                
+                                switch selectedIndex {
+                                case 0:
+                                    NavigationStack {
+                                        MagazineMainView(userViewModel: userVM, magazineVM: magazineVM)
+                                    }
+                                case 1:
+                                    NavigationStack {
+                                        CommunityView(communityVM: communityVM)
+                                    }
+                                case 2:
+                                    NavigationStack {
+                                        CommunityView(communityVM: communityVM)
+                                    }
+                                case 3:
+                                    NavigationStack {
+                                        MapView(clikedMagazineData: clikedMagazineData, userLatitude: userLatitude, userLongitude: userLongitude)
+                                    }
+                                case 4:
+                                    NavigationStack {
+                                        MyPageView(magazineDocument: magazineVM.userPostsFilter(magazineData: magazineVM.magazines, userPostedArr: userVM.postedMagazineID), boomarkedMagazineDocument: magazineVM.userBookmarkedPostsFilter(magazineData: magazineVM.magazines, userBookmarkedPostedArr: userVM.bookmarkedMagazineID), bookmarkedCommunityDoument: communityVM.userBookmarkedCommunityFilter(communityData: communityVM.communities, userBookmarkedCommunityArr: userVM.bookmarkedCommunityID))
+                                    }
+                                default:
+                                    NavigationStack {
+                                        VStack {
+                                            SelectPostView(presented: $presented,
+                                                           communityVM: communityVM,
+                                                           updateNumber: updateNumber,userLatitude: userLatitude , userLongitude: userLongitude)
+                                        }
                                     }
                                 }
+                                Spacer()
+                            }
+                            if isZooming == false {
+                                
+                                HStack {
+                                    ForEach(0..<5, id: \.self) { number in
+                                        Button {
+                                            if number == 2 {
+                                                presented.toggle()
+                                            } else {
+                                                self.selectedIndex = number
+                                            }
+                                        } label: {
+                                            if number == 2 {
+                                                Image(systemName: icons[number])
+                                                    .font(.title2)
+                                                    .foregroundColor(.white)
+                                                    .frame(width: 60, height: 60)
+                                                    .background(.black)
+                                                    .cornerRadius(30)
+                                            }
+                                            else {
+                                                VStack{
+                                                    Image(systemName: icons[number])
+                                                        .font(.title2)
+                                                        .foregroundColor(selectedIndex == number ? .black : Color(UIColor.lightGray))
+                                                    Text(labels[number])
+                                                        .font(.caption)
+                                                        .foregroundColor(selectedIndex == number ? .black : Color(UIColor.lightGray))
+                                                }
+                                                .frame(width: 67, height: 60)
+                                            }
+                                        }
+                                    }
+                                }
+                                
+                                .transition(.move(edge: .bottom))
                             }
                         }
-                        
-                        .transition(.move(edge: .bottom))
+                        .animation(.default , value: isZooming)
                     }
                 }
-                .animation(.default , value: isZooming)
+                .edgesIgnoringSafeArea(.top)    // <- 지도 때문에 넣음
+                .tint(.black)
+                .onAppear{
+                    /// 처음부터 마커 데이터를 가지고 있으면 DispatchQueue를 안해도 되지 않을까?
+                    magazineVM.fetchMagazine()
+                    communityVM.fetchCommunity()
+                    userVM.fetchCurrentUser(userID: Auth.auth().currentUser?.uid ?? "")
+                    
+                    //                    magazineVM.updateMagazine()
+                }
+                .ignoresSafeArea(.keyboard)
+                //        .splashView {
+                //            ZStack{
+                //                SplashScreen()
+                //            }
+                //
+                //        }
+            } else {
+                NetworkConnectionView(networkManager: networkManager)
             }
         }
-        .edgesIgnoringSafeArea(.top)    // <- 지도 때문에 넣음
-        .tint(.black)
-        .onAppear{
-            /// 처음부터 마커 데이터를 가지고 있으면 DispatchQueue를 안해도 되지 않을까?
-            magazineVM.fetchMagazine()
-            communityVM.fetchCommunity()
-            userVM.fetchCurrentUser(userID: Auth.auth().currentUser?.uid ?? "")
-
-            //                    magazineVM.updateMagazine()
-        }
-        .ignoresSafeArea(.keyboard)
-        //        .splashView {
-        //            ZStack{
-        //                SplashScreen()
-        //            }
-        //
-        //        }
     }
     
 }
